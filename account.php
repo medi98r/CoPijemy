eb61200a926864f6a4999a5f6b1c5eb2bf8d41ee
Bartosz Medoń <85805365+medi98r@users.noreply.github.com>
2021-12-02 14:41:46 +0100

    Add files via upload

---
+++ account.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+  <meta charest="utf-8"/>
+  <title>CoPijemy?</title>
+  <meta name="description" content ="Dzięki nam będziesz wiedział, co dziś pijesz!"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+  <div>
+    <div id="container">
+      <div id="header">
+        <img src="img/logo.png" alt="Błąd" class="image_1">
+      </div>
+      <div id="navigation">
+        <ol>
+          <li><a href="index.php">Strona główna</a></li>
+          <li><a href=#>O nas</a></li>
+          <li><a href=#>Przepisy</a></li>
+          <li><a href=#>Konto premium</a></li>
+          <li><a href=#>Zarządzanie kontem</a></li>
+        </ol>
+      </div>
+	  <div id="loginpanel">
+		<br/><br/><br/><br/><br/>
+		<form action="login.php" method="post">
+		Login/e-mail:<br/><input type="text" name="login"/> <br/>
+		Hasło:<br/><input type="password" name="haslo"/> <br/>
+		Pin:<br/><input type="password" name="pin"/> <br/>
+		<input type="submit" value="Zaloguj się"/>
+		</form>
+	  
+	  </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+  <meta charest="utf-8"/>
+  <title>CoPijemy?</title>
+  <meta name="description" content ="Dzięki nam będziesz wiedział, co dziś pijesz!"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+  <div>
+    <div id="container">
+	
+      <div id="header">
+        <img src="img/logo.png" alt="Błąd" class="image_1">
+      </div>
+	  
+      <div id="navigation">
+        <ol>
+          <li><a href="index.php">Strona główna</a></li>
+          <li><a href=#>O nas</a></li>
+          <li><a href=#>Przepisy</a></li>
+          <li><a href=#>Konto premium</a></li>
+          <li><a href="account.php">Zarządzanie kontem</a></li>
+        </ol>
+      </div>
+	  
+	  <div id="mainpage">
+		<div id="text">
+		Dzięki nam już nigdy nie będziesz musiał się głowić nad tym, czego napijesz się na imprezie!<br/><br/><br/>
+		Wystarczy, że podasz nam swoje ulubione składniki, a w ciągu kilku sekund dobierzemy drinka odpowiedniego dla Ciebie!<br/><br/><br/> Uważasz, że to za mało?! Nic straconego - zakup konto premium, dzięki któremu pożegnasz denerwujące Cię reklamy, a także odblokujesz dostęp do niezwykłych dodatków:<br/><br/><br/><b>Obliczanie zawartości alkoholu we krwi<br/><br/><br/>Porady na kaca<br/><br/><br/>Kontrola ilości spożytego alkoholu</b>
+		</div>
+		<div id="images">
+		<img src="img/question.jpg" alt="Błąd" class="image_2">
+		<div class="overlay">
+			<div class="overlay.on">Już nie musisz zachodzić w głowę!</div>
+		</div>
+	  </div>
+    </div>
+  </div>
+</body>
+</html>
