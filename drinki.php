ba80f5b185aefeb320f1e94cce692caf4c2e2115
Bartosz Medoń <85805365+medi98r@users.noreply.github.com>
2022-01-29 17:16:15 +0100

    Add files via upload

---
+++ drinki.php
@@ -0,0 +1,33 @@
+<?php
+
+	public class Drink
+	{
+		public $nazwa;
+		public $stopien_trudnosci;
+		public $moc;
+		public $przepis;
+	}
+	
+	public class JaggerBomb extends Drink
+	{
+		public $skladnik_1;
+		public $skladnik_2;
+	}
+	
+	public class JackBull extends Drink
+	{
+		public $skladnik_1;
+		public $skladnik_2;
+		public $skladnik_3;
+	}
+	
+	public class LongIslandIceTea extends Drink
+	{
+		public $skladnik_1;
+		public $skladnik_2;
+		public $skladnik_3;
+		public $skladnik_4;
+		public $skladnik_5;
+	}
+	
+?>
---
+++ john.php
@@ -0,0 +1,39 @@
+﻿
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+  <meta charest="utf-8"/>
+  <title>CoPijemy?</title>
+  <meta name="description" content ="Dzięki nam będziesz wiedział, co dziś pijesz!"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" type="text/css"/>
+  <style>
+ @font-face {
+	font-family: Roboto-MediumItalic;
+	src:url(font/Roboto-MediumItalic.ttf);
+  }
+  </style>
+</head>
+<body>
+  <div>
+    <div id="container">
+	
+      <div id="header">
+        <img src="img/logo.png" alt="Błąd" class="image_1">
+      </div>
+	  
+      <div id="navigation">
+       <b><ol>
+          <li><a href="index.php">STRONA GŁÓWNA</a></li>
+          <li><a href=#>O NAS</a></li>
+          <li><a href="przepisy.php">PRZEPISY</a></li>
+          <li><a href=#>PREMIUM</a></li>
+          <li><a href="account.php">TWOJE KONTO</a></li>
+        </ol></b>
+      </div>
+	  <div id="recipe">
+	  </div>
+   </div>
+</body>
+</html>
