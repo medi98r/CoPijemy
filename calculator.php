765f3cab8aaa3e6c8bb30de56aebaada67c65dc7
Bartosz Medoń <85805365+medi98r@users.noreply.github.com>
2022-01-30 18:26:13 +0100

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,55 @@
+﻿
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+  <meta charest="utf-8"/>
+  <title>CoPijemy?</title>
+  <meta name="description" content ="Dzięki nam będziesz wiedział, co dziś pijesz!"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" type="text/css"/>
+  <style>
+ @font-face {
+	font-family: Roboto-MediumItalic;
+	src:url(font/Roboto-MediumItalic.ttf);
+  }
+  </style>
+</head>
+<body>
+  <div>
+    <div id="container">
+	
+      <div id="header">
+        <img src="img/logo.png" alt="Błąd" class="image_1">
+      </div>
+	  
+      <div id="navigation">
+       <b><ol>
+          <li><a href="index.php">STRONA GŁÓWNA</a></li>
+          <li><a href=#>O NAS</a></li>
+          <li><a href="przepisy.php">PRZEPISY</a></li>
+          <li><a href=#>PREMIUM</a></li>
+          <li><a href="account.php">TWOJE KONTO</a></li>
+        </ol></b>
+      </div>
+	  <div id = "calculator">
+		<select id="Płeć">
+			<option>Mężczyzna</option>
+			<option>Kobieta</option>
+		</select>
+		<input type="text" id="Waga" value="Waga">
+		</input>
+		<input type="text" id="Wiek" value="Wiek ">
+		</input>
+		<input type="text" id="objetosc" value="alk. w ml">
+		</input>
+		<input type = "submit" id ="przycisk">
+	  </div>
+	  <div id = "result">
+		<input type="wynik" id="wynik">
+		</input>
+	  </div>
+   </div>
+   <script src="calculator.js"></script>
+</body>
+</html>
